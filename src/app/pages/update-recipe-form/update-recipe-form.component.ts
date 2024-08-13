e9a91ee93559cbd8170162d66a6dc9d15aa21218
upit <upit.upit@enigma.com>
2024-08-13 19:31:58 +0700

    [add] - menambahkan fitur update resep

---
+++ src/app/pages/update-recipe-form/update-recipe-form.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+
+@Component({
+  selector: 'app-update-recipe-form',
+  standalone: true,
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatRadioModule
+  ],
+  templateUrl: './update-recipe-form.component.html',
+  styleUrl: './update-recipe-form.component.scss'
+})
+export class UpdateRecipeFormComponent {
+  recipeItem:any = {
+    title:"Bakso",
+    description: "nice food",
+    foodType:'veg',
+    image:'image'
+  }
+
+  onSubmit(){
+    console.log("values", this.recipeItem)
+  }
+}
