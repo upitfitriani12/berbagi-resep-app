de46f4dc74c327f51c1b4a62dc6e5a320d00bbae
upit <upit.upit@enigma.com>
2024-08-13 19:05:01 +0700

    [add] - update feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { NavbarComponent } from "./pages/navbar/navbar.component";
+import { FooterComponent } from "./pages/footer/footer.component";
+import { HomePageComponent } from "./pages/home-page/home-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NavbarComponent,
+    FooterComponent,
+    HomePageComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/pages/home-page/home-page.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { RecipeCardComponent } from "../recipe-card/recipe-card.component";
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { CreateRecipeFormComponent } from '../create-recipe-form/create-recipe-form.component';
+
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+  imports: [RecipeCardComponent, MatIconModule, MatButtonModule],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.scss'
+})
+export class HomePageComponent {
+  recipes=[1,1,1,1,11,1]
+
+  constructor(public dialog: MatDialog){}
+
+  handleOpenCreateRecipeForm(){
+    this.dialog.open(CreateRecipeFormComponent)
+  }
+
+}
